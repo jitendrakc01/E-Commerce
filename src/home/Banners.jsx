bb8aa3c0f84ee4106f42ad97ef68325cec28567b
jitendra kc <jitendrachettri894@gmail.com>
2024-08-16 15:48:23 +0545

    Initial commit

---
+++ src/home/Banners.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import productData from "../products.json"
+import { Link } from "react-router-dom";
+import SelectedCategory from "../components/SelectedCategory";
+
+const title = <h2>search Your One From<span>Thousand</span>  of Products</h2>;
+
+const desc = "We have the largest collection of products";
+const bannerList = [
+  {
+    iconName: "icofont-users-alt-4",
+    text: "1.5 Million Customers",
+  },
+  {
+    iconName: "icofont-notification",
+    text: "More then 2000 Marchent",
+  },
+  {
+    iconName: "icofont-globe",
+    text: "Buy Anything Online",
+  },
+];
+
+const Banners = () => {
+        const [searchInput, setSearchInput] = useState("");
+        const [filteredProduct, setfilteredProducts] = useState(productData);
+        // console.log(productData)
+
+
+
+       //search funtionality
+   const handlesearch = e => {
+        
+        const searchTerm = e.target.value;
+        setSearchInput(searchTerm);
+
+        //filtering products base on search
+        const filtered = productData.filter(
+                (product) => product.name.toLowerCase()
+                .includes(searchTerm.toLowerCase()));
+        
+          setfilteredProducts(filtered);      
+
+   }
+
+  return <div className="banner-section style-4">
+        <div className="container">
+                <div className="banner-content">
+                        {title}
+                        <form>
+
+                 <SelectedCategory select={"all"}/>
+
+                
+                        
+                        
+                        <input type="text" name="search" id=
+                        "search" placeholder="search your product" value=
+                        {searchInput} onChange={handlesearch} />
+                         <button type="sumit">
+                         <i className="icofont-search"></i>
+                         </button>
+                        </form>
+                        <p>{desc}</p>
+                        <ul className="lab-ul">
+                        {
+                                searchInput && filteredProduct.map((product,i) => <li key={i}>
+                                 <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                                </li> )
+                        }
+
+                        </ul>
+
+                </div>
+
+        </div>
+
+  </div>;
+};
+
+export default Banners;
